406620c7bc92ce9ebf1e26925eb5f437c52de9ee
nihalasati <nihal.asati@nineleaps.com>
2020-09-20 12:11:34 +0530

    [Update] index.html and hibernate.annotations.cfg.xml

--- target/classes/product_cart.sql
+++ target/classes/product_cart.sql
@@ -1,14 +1,13 @@
 use onlineEcommerce;
-create table user(
-	user_id int(11) NOT NULL AUTO_INCREMENT,
-	name varchar(40) NOT NULL,
-	email varchar(40) NOT NULL,
-	PRIMARY KEY(user_id),
+create table `e_cart`(
+	cart_id int(11) NOT NULL AUTO_INCREMENT,
+	cart_total decimal(10,2) NOT NULL,
+	PRIMARY KEY(cart_id),
 	add_id int(11) NOT NULL,
 	KEY user_fk (add_id),
 	CONSTRAINT user_fk FOREIGN KEY (add_id) REFERENCES address(add_id);
 );
-create table address(
+create table product(
 	add_id int(11) NOT NULL AUTO_INCREMENT,
 	street varchar(40) NOT NULL,
 	city varchar(40) NOT NULL,
